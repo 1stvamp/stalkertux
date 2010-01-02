e80c778393f800adb131bd4c8ed7350b3fc92aea
Wesley Mason <wes@toottoot.(none)>
2010-01-02 23:41:29 +0000

    Cleaned up code and added better quitting

--- stalker.py
+++ stalker.py
@@ -1,25 +1,22 @@
-import Image, ImageDraw, motion, time, pygame, sys, random, psyco
+import Image, motion, pygame, sys, psyco, opencv
 from pygame.locals import *
-import opencv
-#this is important for capturing/displaying images
 from opencv import highgui
 
-COLORTHRESHOLD = 50 #color threshold for motion detection, the higher the more strict (avg. 50 is ok)
-SIZE = 640,480 # resolution of the screen (and camera, but can be seperated), needs to be 4/3 (I think)
-CSIZE = 160,120 #compressed size, needs to be 4/3 (widescreen hack?)
+# Color threshold for motion detection, the higher the more strict (avg. 50 is ok)
+COLOR_THRESHOLD = 50
+# Resolution of the screen (and camera, but can be seperated), needs to be 4/3 (I think)
+SIZE = 640,480
+# Compressed size, needs to be 4/3 (widescreen hack?)
+CSIZE = 160,120
 
-#init vars
-##########
-psyco.full() #PSYCO speeds up python
-pygame.init() #We need to initialize pygame early on, so that certain stuff works (loading sound..)
-ratio = SIZE[0]/CSIZE[0] #ratio of compression
+psyco.full()
+pygame.init()
+# Ratio of compression
+ratio = SIZE[0] / CSIZE[0]
 
-#new empty images, we will be using them in the main loop
-#ni = newest image
-#nci = newest compressed image
-#oci = older compressed image
-cci = Image.new("RGB",CSIZE,(255,255,255))
-ni = Image.new("RGB",SIZE,(255,255,255))
+# Empty images for comparison
+cci = Image.new("RGB", CSIZE, (255,255,255))
+ni = Image.new("RGB", SIZE, (255,255,255))
 oci = cci
 
 
@@ -28,50 +25,47 @@ def get_image():
     im = highgui.cvQueryFrame(camera)
     # Add the line below if you need it (Ubuntu 8.04+)
     #im = opencv.cvGetMat(im)
-    #convert Ipl image to PIL image
+    # Convert Ipl image to PIL image
     return opencv.adaptors.Ipl2PIL(im)
 
 fps = 30.0
 pygame.init()
 window = pygame.display.set_mode(SIZE)
-pygame.display.set_caption("WebCam Demo")
+pygame.display.set_caption("Stalker Tux")
 screen = pygame.display.get_surface()
 
 while True:
     events = pygame.event.get()
     for event in events:
-        if event.type == QUIT or event.type == KEYDOWN:
+        if event.type == QUIT or \
+            (event.type == pygame.KEYDOWN
+             and event.key == pygame.K_ESCAPE):
+
             sys.exit(0)
+
     im = get_image()
     pg_img = pygame.image.frombuffer(im.tostring(), im.size, im.mode)
     screen.blit(pg_img, (0,0))
 
-    #Get the images
-    ###############
-    #oci and nci are used for motion detection, they don't show up on screen
-    #if you want to show something use ni
-    oci = cci #make the current compressed image the old one
-    ni = im #get new image for compression
-    #ni = ni.transpose(Image.FLIP_LEFT_RIGHT) #Flip image for mirror movement, this way topleft == (0,0)
-    cci = ni.resize(CSIZE,Image.BILINEAR) #compress the new image and make it the current one
+    # Make the current compressed image the old one
+    oci = cci
+    # Compress the new image and make it the current one
+    cci = im.resize(CSIZE, Image.BILINEAR)
 
-    #Get motion from images
-    #######################
-    motionArray = motion.getMotionArray(oci,cci,COLORTHRESHOLD) #compare the images and get the array of pixels with difference
+    # Compare the images and get the array of pixels with difference
+    motionArray = motion.getMotionArray(oci, cci, COLOR_THRESHOLD)
     if len(motionArray) < 1:
         continue
-    motionPoint = motion.getMotionPoint(motionArray) #calculate the avarage point
-    if motionPoint[0] > 0 and motionPoint[1] > 0: #chek X and Y values
-        #if a real avarage point of motion has been returned
-        wp = motionPoint
-        px = wp[0]*ratio #multiply by compression ratio to get the position
-        py = wp[1]*ratio #of the coordinate on the uncompressed image
+    # Calculate the avarage point
+    motionPoint = motion.getMotionPoint(motionArray)
+    if motionPoint[0] > 0 and motionPoint[1] > 0:
+        # Multiply by compression ratio to get the position
+        px = motionPoint[0] * ratio
+        # Of the coordinate on the uncompressed image
+        py = motionPoint[1] * ratio
         wp = (px,py)
 
-    #Draw everything
-    #################
+    # Draw everything
     pygame.draw.lines(screen, (0,255,0), 0, [[0,0], wp,])
     pygame.display.flip()
-    pygame.time.delay(int(1000 * 1.0/fps))
-
-
+    pygame.time.delay(int(1000 * 1.0 / fps))
