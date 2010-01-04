cf34dab0806123d4f04774bd3f99e7e45be720a9
Wesley Mason <wes@toottoot.(none)>
2010-01-04 13:27:59 +0000

    Converted GUI and detection over to using OpenCV entirely

---
+++ face.py
@@ -0,0 +1,28 @@
+import sys
+from opencv import cv
+
+def detect(image, cascade_file='haarcascade_frontalface_alt.xml'):
+    image_size = cv.cvGetSize(image)
+
+    # create grayscale version
+    grayscale = cv.cvCreateImage(image_size, 8, 1)
+    cv.cvCvtColor(image, grayscale, cv.CV_BGR2GRAY)
+
+    # create storage
+    storage = cv.cvCreateMemStorage(0)
+    cv.cvClearMemStorage(storage)
+
+    # equalize histogram
+    cv.cvEqualizeHist(grayscale, grayscale)
+
+    # detect objects
+    cascade = cv.cvLoadHaarClassifierCascade(cascade_file, cv.cvSize(1,1))
+    faces = cv.cvHaarDetectObjects(grayscale, cascade, storage, 1.2, 2, cv.CV_HAAR_DO_CANNY_PRUNING, cv.cvSize(50, 50))
+
+    if faces:
+        print 'face detected!'
+        for i in faces:
+            cv.cvRectangle(image, cv.cvPoint( int(i.x), int(i.y)),
+                         cv.cvPoint(int(i.x + i.width), int(i.y + i.height)),
+                         cv.CV_RGB(0, 255, 0), 3, 8, 0)
+
--- stalker.py
+++ stalker.py
@@ -1,28 +1,16 @@
-import Image, motion, pygame, sys, psyco, opencv
+import Image, pygame, sys, psyco, opencv, face
 from pygame.locals import *
 from opencv import highgui
 from tuxisalive.api.sh import *
 
-# Color threshold for motion detection, the higher the more strict (avg. 50 is ok)
-COLOR_THRESHOLD = 20
 # Resolution of the screen (and camera, but can be seperated), needs to be 4/3 (I think)
-SIZE = 640,480
-# Compressed size, needs to be 4/3 (widescreen hack?)
-CSIZE = 160,120
+size = 640,480
 # Division of the screen to count as "walking" motion to trigger tux
-MOTION_BLOCK = SIZE[0] / 10
+motion_block = size[0] / 10
 # Frames per second
-FPS = 10.0
+fps = 10
 
 psyco.full()
-pygame.init()
-# Ratio of compression
-ratio = SIZE[0] / CSIZE[0]
-
-# Empty images for comparison
-cci = Image.new("RGB", CSIZE, (255,255,255))
-ni = Image.new("RGB", SIZE, (255,255,255))
-oci = cci
 
 
 def move_tux_right(amount):
@@ -39,61 +27,33 @@ def move_tux_left(amount):
     print amount
 
 camera = highgui.cvCreateCameraCapture(0)
-def get_image():
-    im = highgui.cvQueryFrame(camera)
-    # Add the line below if you need it (Ubuntu 8.04+)
-    #im = opencv.cvGetMat(im)
-    # Convert Ipl image to PIL image
-    return opencv.adaptors.Ipl2PIL(im)
 
 last_x = None
-pygame.init()
-window = pygame.display.set_mode(SIZE)
-pygame.display.set_caption("Stalker Tux")
-screen = pygame.display.get_surface()
 
 while True:
-    events = pygame.event.get()
-    for event in events:
-        if event.type == QUIT or \
-            (event.type == pygame.KEYDOWN
-             and event.key == pygame.K_ESCAPE):
+    highgui.cvNamedWindow('Camera', 1)
+    im = highgui.cvQueryFrame(camera)
+    if im is None:
+        break
+    # mirror
+    opencv.cv.cvFlip(im, None, 1)
 
-            sys.exit(0)
+    face.detect(im, 'haarcascade_profileface.xml')
 
-    im = get_image()
-    pg_img = pygame.image.frombuffer(im.tostring(), im.size, im.mode)
-    screen.blit(pg_img, (0,0))
+    # display webcam image
+    highgui.cvShowImage('Camera', im)
 
-    # Make the current compressed image the old one
-    oci = cci
-    # Compress the new image and make it the current one
-    cci = im.resize(CSIZE, Image.BILINEAR)
 
-    # Compare the images and get the array of pixels with difference
-    motionArray = motion.getMotionArray(oci, cci, COLOR_THRESHOLD)
-    if len(motionArray) < 1:
-        continue
-    # Calculate the avarage point
-    motionPoint = motion.getMotionPoint(motionArray)
-    if motionPoint[0] > 0 and motionPoint[1] > 0:
-        # Multiply by compression ratio to get the position
-        px = motionPoint[0] * ratio
-        # Of the coordinate on the uncompressed image
-        py = motionPoint[1] * ratio
-        wp = (px,py)
+#    if last_x is not None:
+#        test_x = px - last_x
+#        if test_x < 0:
+#            if test_x <= (MOTION_BLOCK * -1):
+#                move_tux_left(int(test_x / MOTION_BLOCK) * -1)
+#        elif test_x > 0:
+#            if test_x >= MOTION_BLOCK:
+#                move_tux_right(int(test_x / MOTION_BLOCK))
+#    last_x = None
 
-    if last_x is not None:
-        test_x = px - last_x
-        if test_x < 0:
-            if test_x <= (MOTION_BLOCK * -1):
-                move_tux_left(int(test_x / MOTION_BLOCK) * -1)
-        elif test_x > 0:
-            if test_x >= MOTION_BLOCK:
-                move_tux_right(int(test_x / MOTION_BLOCK))
-    last_x = px
-
-    # Draw everything
-    pygame.draw.lines(screen, (0,255,0), 0, [[0,0], wp,])
-    pygame.display.flip()
-    pygame.time.delay(int(1000 * 1.0 / FPS))
+    if highgui.cvWaitKey(fps) >= 0:
+        break;
+highgui.cvDestroyWindow('Camera')
